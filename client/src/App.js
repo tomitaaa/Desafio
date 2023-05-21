0a7aea66d0ddc613857065ee7e6d5e3736d5b0a1
Tomita <gabrieltomita@hotmail.com>
2023-05-21 20:28:08 -0300

    adicionando o botão para cancelar a inserção de dados e movendo o listar pessoas para a aba relatórios

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 function App() {
   const [activeTab, setActiveTab] = useState("cadastro");
-  const [showCadastroFields, setShowCadastroFields] = useState(false);
+  const [showCadastro, setShowCadastro] = useState(false);
   const [cidades, setCidades] = useState([]);
   const [bairros, setBairros] = useState([]);
   const [Nome, setNome] = useState("");
@@ -21,11 +21,20 @@ function App() {
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
-    if (tab === "cadastro") {
-      setShowCadastroFields(true);
-    } else {
-      setShowCadastroFields(false);
-    }
+    setShowCadastro(tab === "cadastro");
+  };
+
+  const handleCancel = () => {
+    setNome("");
+    setID(0);
+    setCidade("");
+    setBairro("");
+    setCEP("");
+    setEndereco("");
+    setNumero("");
+    setComplemento("");
+    setTelefone("");
+    setEmail("");
   };
 
   const addCadastro = () => {
@@ -85,88 +94,104 @@ function App() {
       </div>
 
       <div className="Content">
-        {activeTab === "cadastro" && showCadastroFields && (
-          <div className="CadastroContent">
-            <label>Nome:</label>
-            <input type="text" onChange={(event) => setNome(event.target.value)} />
-            <label>ID:</label>
-            <input type="number" onChange={(event) => setID(event.target.value)} />
+        {showCadastro && (
+          <div className="Cadastros">
+            <div className="CadastroContent">
+              <label> Nome:</label>
+              <input
+                type="text"
+                value={Nome}
+                onChange={(event) => setNome(event.target.value)}
+              />
+              <label> ID:</label>
+              <input
+                type="number"
+                value={ID}
+                onChange={(event) => setID(event.target.value)}
+              />
+              <label> Cidade:</label>
+              <input
+                type="text"
+                value={Cidade}
+                onChange={(event) => setCidade(event.target.value)}
+              />
+              <label> Bairro:</label>
+              <input
+                type="text"
+                value={Bairro}
+                onChange={(event) => setBairro(event.target.value)}
+              />
+              <label> CEP:</label>
+              <input
+                type="text"
+                value={CEP}
+                onChange={(event) => setCEP(event.target.value)}
+              />
+              <label> Endereço:</label>
+              <input
+                type="text"
+                value={Endereco}
+                onChange={(event) => setEndereco(event.target.value)}
+              />
+              <label> Número:</label>
+              <input
+                type="text"
+                value={Numero}
+                onChange={(event) => setNumero(event.target.value)}
+              />
+              <label> Complemento:</label>
+              <input
+                type="text"
+                value={Complemento}
+                onChange={(event) => setComplemento(event.target.value)}
+              />
+              <label> Telefone:</label>
+              <input
+                type="text"
+                value={Telefone}
+                onChange={(event) => setTelefone(event.target.value)}
+              />
+              <label> Email:</label>
+              <input
+                type="text"
+                value={Email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
 
-            <div className="InputContainer">
-              <label>Cidade:</label>
-              <input type="text" onChange={(event) => setCidade(event.target.value)} />
-              <select value={Cidade} onChange={(event) => setCidade(event.target.value)}>
-                <option value="">Selecione</option>
-                {cidades.map((cidade) => (
-                  <option key={cidade} value={cidade}>
-                    {cidade}
-                  </option>
-                ))}
-              </select>
+              <div className="Buttons">
+                <button onClick={addCadastro}>Cadastrar</button>
+                <button onClick={handleCancel}>Cancelar</button>
+              </div>
             </div>
-
-            <div className="InputContainer">
-              <label>Bairro:</label>
-              <input type="text" onChange={(event) => setBairro(event.target.value)} />
-              <select value={Bairro} onChange={(event) => setBairro(event.target.value)}>
-                <option value="">Selecione</option>
-                {bairros.map((bairro) => (
-                  <option key={bairro} value={bairro}>
-                    {bairro}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <label>CEP:</label>
-            <input type="text" onChange={(event) => setCEP(event.target.value)} />
-            <label>Endereço:</label>
-            <input type="text" onChange={(event) => setEndereco(event.target.value)} />
-            <label>Número:</label>
-            <input type="text" onChange={(event) => setNumero(event.target.value)} />
-            <label>Complemento:</label>
-            <input type="text" onChange={(event) => setComplemento(event.target.value)} />
-            <label>Telefone:</label>
-            <input type="text" onChange={(event) => setTelefone(event.target.value)} />
-            <label>Email:</label>
-            <input type="text" onChange={(event) => setEmail(event.target.value)} />
-
-            <button onClick={addCadastro}>Cadastrar</button>
           </div>
         )}
 
         {activeTab === "movimento" && (
           <div className="Movimento">
-            {/* Implemente o conteúdo da aba de movimento */}
+            {/* Conteúdo da aba de Movimento */}
           </div>
         )}
 
         {activeTab === "relatorios" && (
           <div className="Relatorios">
-            {/* Implemente o conteúdo da aba de relatórios */}
+            <button onClick={getPessoas}>Listar Pessoas</button>
+
+            {listaCadastro.map((val, key) => (
+              <div className="Pessoa" key={val.ID}>
+                <h3>Nome: {val.Nome}</h3>
+                <h3>ID: {val.ID}</h3>
+                <h3>Cidade: {val.Cidade}</h3>
+                <h3>Bairro: {val.Bairro}</h3>
+                <h3>CEP: {val.CEP}</h3>
+                <h3>Endereço: {val.Endereco}</h3>
+                <h3>Número: {val.Numero}</h3>
+                <h3>Complemento: {val.Complemento}</h3>
+                <h3>Telefone: {val.Telefone}</h3>
+                <h3>E-mail: {val.Email}</h3>
+              </div>
+            ))}
           </div>
         )}
-      </div>
-
-      <div className="Pessoas">
-        <button onClick={getPessoas}>Listar Pessoas</button>
-
-        {listaCadastro.map((val, key) => {
-          return (
-            <div className="Pessoa" key={val.ID}>
-              <h3>Nome: {val.Nome}</h3>
-              <h3>ID: {val.ID}</h3>
-              <h3>Cidade: {val.Cidade}</h3>
-              <h3>Bairro: {val.Bairro}</h3>
-              <h3>CEP: {val.CEP}</h3>
-              <h3>Endereço: {val.Endereco}</h3>
-              <h3>Número: {val.Numero}</h3>
-              <h3>Complemento: {val.Complemento}</h3>
-              <h3>Telefone: {val.Telefone}</h3>
-              <h3>E-mail: {val.Email}</h3>
-            </div>
-          );
-        })}
       </div>
     </div>
   );
